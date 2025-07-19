6bd835a6137c1c04e732a2a3465db179a1ca7e48
Tomasz Sas <tomaszsas.dev@gmail.com>
2025-07-19 12:33:50 +0200

    add variables

---
+++ 2025-07-19/more_advance_print.py
@@ -0,0 +1,25 @@
+# This is examples print function
+
+print()
+
+print("To jest tekst przykładowy")
+
+print(23)
+
+# Przyklady operacji matematycznych w funkcji print 
+x = 30 
+y = 20
+z = 2
+print("To jest zmienna x = ", x)
+print("To jest zmienna y = ", y)
+print("suma = ", x + y)
+print("To jest odejmowanie [30 - 20]", x - y)
+print("To jest mnożenie [30 * 20]", x * y)
+print("To jest dzielenie z resztą [30 / 20]", x / y)
+# Dzielenie bez reszty 
+print("To jest dzielenie bez reszty [30 //20]", x // y)
+# Potęgowanie
+print("To jest potęgoweanie [2 ** 2]", z ** 2)
+# Dzielenie modulo 
+print("To jest reszta z dzielenia [4 % 2]", 4 % 2)
+print("To jest reszta z dzielenia [5 % 2]", 5 % 2)
---
+++ 2025-07-19/variables.py
@@ -0,0 +1,23 @@
+var1 = "Test"
+var2 = 23
+var3 = [1, 2, 4]
+var4 = (1, 2, 3, 4)
+var4 = {
+    "key1": 1, 
+    "key2": 2,
+    "Key3": "Text"
+}
+
+print(var4)
+
+# Nazywanie zmiennych
+counter_page = 1
+
+# Wielkości liter mają znaczenie
+A = 10
+a = 11
+
+print(A)
+print(a)
+
+int = 1
