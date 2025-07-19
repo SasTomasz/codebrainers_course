35db6c2687fafb7bd0fbf0bf79150480a369445c
Tomasz Sas <tomaszsas.dev@gmail.com>
2025-07-19 14:22:43 +0200

    stringi

---
+++ 2025-07-19/strings.py
@@ -0,0 +1,17 @@
+first = "a very short text"
+second = "very long text"
+# Dodawanie = konkatenacja = łączenie ciągów znaków 
+third = first + second 
+print(third)
+# Mnożyc = zwielokrotniac ciag znakow
+fourth = "a"
+print(fourth * 50)
+fourth += "a"
+print(fourth)
+citiation = "This is 'citate'"
+citiation1 = 'He doesn\'t'
+print("\n" * 5)
+print("\t", citiation1)
+print("\a" * 5)
+print(r"C:\Users\tomas\PythonProjects\Codebrainers\2025-07-19")
+print("\u20ac") # unicode - znak euro
