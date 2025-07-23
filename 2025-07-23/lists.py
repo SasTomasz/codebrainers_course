7d96fc0a2981d1545ace319ce5c6faa531e76e1a
tomasz_sas <tomaszsas.dev@gmail.com>
2025-07-23 09:14:43 +0200

    concatinating lists

---
+++ 2025-07-23/lists.py
@@ -0,0 +1,7 @@
+# As above example shows, you can concatenate lists in Python using the `+` operator.
+# This allows you to combine multiple lists into one.
+list1 = [1, 2, 3, 4, 5]
+list2 = [6, 7, 8, 9, 10]
+list3 = list1 + [11, 12, 13, 14, 15] + list2
+
+print(list3)
