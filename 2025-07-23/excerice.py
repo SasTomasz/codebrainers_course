e1310de4b7db4179536704de02d1da7c9b0d5650
Tomasz Sas <tomaszsas.dev@gmail.com>
2025-07-23 20:30:41 +0200

    Add excercise instructions for homework

---
+++ 2025-07-23/excerice.py
@@ -0,0 +1,18 @@
+# Ćwiczenie nr 1 - inna propozycja:
+# Mając dany ciąg znaków o nieparzystej długości większej niż 7,
+# zwróć nowy łańcuch złożony z trzech środkowych znaków tego ciągu.
+
+
+# Ćwiczenie nr 2:
+# Mając dane dwa ciągi znaków s1 i s2, utwórz nowy ciąg znaków,
+# w którym s2 zostanie wstawiony dokładnie w środek s1.
+
+# Ćwiczenie nr 3:
+# Mając dane dwa łańcuchy znaków s1 i s2 o nieparzystej długości,
+# zwróć nowy łańcuch złożony z:
+# - pierwszego, środkowego i ostatniego znaku z s1,
+# - pierwszego, środkowego i ostatniego znaku z s2.
+
+
+# Ćwiczenie nr 4:
+# Odwróć podany ciąg znaków.
