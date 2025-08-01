ae34e520c3ef9aed367d79e222b05fb45d42c844
tomasz_sas <tomaszsas.dev@gmail.com>
2025-08-01 08:44:11 +0200

    Add resolution of excercise 8 from notebook 005

---
+++ 2025-07-24/ex8.py
@@ -0,0 +1,9 @@
+# ### Ćwiczenie nr 8:
+# Napisz program, który:
+# * Wyświetli 10 razy "Hello world!"
+# * Następnie wyświetli napis "Gotowe"
+
+for i in range(10):
+    print("Hello world!")
+
+print("Gotowe")
