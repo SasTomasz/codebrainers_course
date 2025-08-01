617a325c40a45d95d9a5436a106e0b3a703faac7
tomasz_sas <tomaszsas.dev@gmail.com>
2025-08-01 09:02:32 +0200

    Add resolution of excercise 10 from notebook 005

---
+++ 2025-07-24/ex10.py
@@ -0,0 +1,17 @@
+# ### Ćwiczenie nr 10:
+# Umieść instrukcję `pass`, aby blok `if` nie zgłaszał błędu
+
+# ```python
+# name = input("Proszę wpisać swoje imię.")
+
+# if len(name) > 0:
+#     print(name)
+# else:
+# ```
+
+name = input("Proszę wpisać swoje imię.")
+
+if len(name) > 0:
+    print(name)
+else:
+    pass
