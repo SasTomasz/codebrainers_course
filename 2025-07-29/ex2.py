b3299285909c5ac60139150a19754d5a3ad1d9e2
Tomasz Sas <tomaszsas.dev@gmail.com>
2025-08-02 07:35:59 +0200

    Add resolution of excercise 2 from notebook 006

---
+++ 2025-07-29/ex2.py
@@ -0,0 +1,12 @@
+# ### Ćwiczenie nr 2:
+
+# Napisz funkcję w Pythonie, która zsumuje wszystkie elementy na liście.
+
+# input: `[1, 2, -8]`
+
+# output: `-5`
+
+def sum_elements_on_list(numbers):
+    return sum(numbers)
+
+print(sum_elements_on_list([1, 2, -8]))
