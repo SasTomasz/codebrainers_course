c99fc49e937f21e49ed3619f167f598d8a6b04bf
Tomasz Sas <tomaszsas.dev@gmail.com>
2025-08-02 15:33:11 +0200

    Add partial resolution of excercise 3 from notebook 008

---
+++ 2025-08-02/ex3-008.py
@@ -0,0 +1,18 @@
+# ### Ćwiczenie nr 3:
+# Napisz program w Pythonie, który znajdzie najdłuższe słowa w pliku tekstowym.
+
+# input: plik `008_file_text.txt`
+
+# output: `['przeznaczenia.', 'programowania,']`
+
+def get_the_longest_word(path):
+    with open(path, "r", encoding="utf-8") as f:
+        content = str(f.read())
+
+    words = content.strip("\n")
+    print(words)
+    words = content.split(" ")
+    print(words)
+    # TODO Continue from here
+
+get_the_longest_word(r"2025-08-02\example1.txt")
---
+++ 2025-08-02/list_conprehensions.py
@@ -0,0 +1,12 @@
+# Napisz program w Pythonie, który usuwa liczby dodatnie z podanej listy liczb
+# Zsumuj liczby ujemne i wydrukuj wartość bezwzględną za pomoca tworzenia listy - ang. list comprehension
+# Wydrukuj wynik
+
+nums = [2, 4, -6, -9, 11, -12, 14, -5, 17, 2, 4]
+
+print(id(nums))
+
+x = [1, 2, 3]
+print(id(x))         # Decimal format
+print(hex(id(x)))    # Hexadecimal format (e.g., 0x7fabc4efc8c0)
+
