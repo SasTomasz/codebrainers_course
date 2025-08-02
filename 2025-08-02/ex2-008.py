2523e3ec0c3a0d0247d5dda1e985016b7fd27069
Tomasz Sas <tomaszsas.dev@gmail.com>
2025-08-02 09:59:07 +0200

    Add resolution excercise 2 from notebook 008

---
+++ 2025-08-02/ex2-008.py
@@ -0,0 +1,9 @@
+# ### Ćwiczenie nr 2:
+# Napisz program w Pythonie, aby odczytać i wyświetlić zawartość pliku w którym
+# piszesz kod
+
+def show_text_from_file(path):
+    with open(path, "r", encoding="utf-8") as f:
+        print(f.read())
+    
+show_text_from_file(r"2025-08-02\example1.txt")
