93228fb163089c760e204a03ba4f91a920a4fc00
Tomasz Sas <tomaszsas.dev@gmail.com>
2025-08-02 06:24:41 +0200

    Add resolution of excercise 13 from notebook 005

---
+++ 2025-07-24/ex13.py
@@ -0,0 +1,11 @@
+# ### Ćwiczenie nr 13:
+# Wypisz pierwsze 10 liczb naturalnych za pomocą pętli `while`
+
+# Mój komentarz:
+# Zgodnie z teorią liczb naturalnych trzeba doprecyzować czy zero uznajemy za liczbą naturalną. 
+# Na potrzeby tego zadania wyłączam 0 ze zbioru liczb naturalnych.
+
+x = 1
+while x <= 10:
+    print(x)
+    x += 1
