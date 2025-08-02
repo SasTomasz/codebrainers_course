c1a4b56adbfe5d8975819b91d5d99ef586452005
Tomasz Sas <tomaszsas.dev@gmail.com>
2025-08-02 07:33:19 +0200

    Add resolution of excercise 1 from notebook 006

---
+++ 2025-07-29/ex1.py
@@ -0,0 +1,12 @@
+# ### Ćwiczenie nr 1:
+
+# Napisz funkcję w Pythonie, która obliczania długości łańcucha.
+
+# Input: `"CodeBrainers"`
+
+# Output: `12`
+
+def compute_length_of_text(text):
+    return len(text)
+
+print(compute_length_of_text("CodeBrainers"))
