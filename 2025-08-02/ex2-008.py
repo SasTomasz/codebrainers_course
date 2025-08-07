cc90c634fd5abca3d0079bfac617b87eaeae814b
tomasz_sas <tomaszsas.dev@gmail.com>
2025-08-07 09:04:29 +0200

    Fix excercise to met requirements from description

--- 2025-08-02/ex2-008.py
+++ 2025-08-02/ex2-008.py
@@ -6,4 +6,4 @@ def show_text_from_file(path):
     with open(path, "r", encoding="utf-8") as f:
         print(f.read())
     
-show_text_from_file(r"2025-08-02\example1.txt")
+show_text_from_file(r"2025-08-02\ex2-008.py")
