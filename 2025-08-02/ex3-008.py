f33eb3d52a63b8f41dd95d2f37c2ec8523ae51e9
tomasz_sas <tomaszsas.dev@gmail.com>
2025-08-11 10:20:37 +0200

    Update story.txt and start resolve excercise using dictionary

--- 2025-08-02/ex3-008.py
+++ 2025-08-02/ex3-008.py
@@ -7,17 +7,18 @@
 
 def get_the_longest_word(path):
     with open(path, "r", encoding="utf-8") as f:
-        content = str(f.read())
+        content = f.read()
         
-    words = content.split(" ")
-    new_words = []
-    longest_word = ""
+    words = content.split()
+    words = [word.strip() for word in words]
+    longest_words = {}
     for word in words:
-        new_word = word.strip("\n")
-        new_words.append(new_word)
-        if len(longest_word) < len(new_word):
-            longest_word = new_word
-    print(new_word)
+        if len(word) in longest_words.keys():
+            longest_words[f"{len(word)}"] = longest_words[f"{len(word)}"].append(word)
+        else:
+            longest_words[f"{len(word)}"] = [word]
+    print(longest_words)
+    
     # TODO Continue from here
 
 get_the_longest_word(r"2025-08-02\story.txt")
